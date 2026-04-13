e4f349ffcf52795247628f6a60454f9b9a449354
stom <tomsteventon@gmail.com>
2026-04-13 17:21:02 +0100

    refactor: move light setupto own script

--- dcl/src/client/index.ts
+++ dcl/src/client/index.ts
@@ -8,6 +8,7 @@ import { SetupUI } from 'src/client/ui'
 import { engine, LightSource, MeshRenderer, Transform } from '@dcl/sdk/ecs'
 import { Color3, Vector3 } from '@dcl/sdk/math'
 import { MessageType, room } from 'src/shared/room'
+import { setupLights } from './lights'
 
 
 export async function initClient() {
@@ -19,39 +20,12 @@ export async function initClient() {
 
 	SetupUI()
 	setupBowlingHostNpc()
+	setupLights()
 
 
 	// DEBUG: auto-join a game on load
 	//room.send(MessageType.REQUEST_JOIN_GAME, 2)
 
 
-	// Add a light above the front desk
-	const light1 = engine.addEntity()
-	Transform.create(light1, { position: Vector3.create(16, 7, 12) })
-	LightSource.create(light1, {
-		type: LightSource.Type.Point({}),
-		color: Color3.White(),
-		intensity: 300000
-	})
-	MeshRenderer.setSphere(light1)
 
-	// Add a light at the back of the bowling alley
-	const light2 = engine.addEntity()
-	Transform.create(light2, { position: Vector3.create(8, 5, 32) })
-	LightSource.create(light2, {
-		type: LightSource.Type.Point({}),
-		color: Color3.White(),
-		intensity: 300000
-	})
-	MeshRenderer.setSphere(light2)
-
-	// Add a light at the back of the bowling alley
-	const light3 = engine.addEntity()
-	Transform.create(light3, { position: Vector3.create(24, 5, 32) })
-	LightSource.create(light3, {
-		type: LightSource.Type.Point({}),
-		color: Color3.White(),
-		intensity: 300000
-	})
-	MeshRenderer.setSphere(light3)
 }
