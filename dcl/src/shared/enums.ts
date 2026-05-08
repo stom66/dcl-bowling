e41ff301e81092d518b2345e00cb6f98751ebbac
stom <tomsteventon@gmail.com>
2026-05-08 13:30:15 +0100

    refactor: rename some enamu values

--- dcl/src/shared/enums.ts
+++ dcl/src/shared/enums.ts
@@ -14,14 +14,14 @@ export enum PlayerStatus {
 }
 
 export enum LanePhase {
-	NONE            = "NONE",              // Not running / idle
-	GAME_STARTING   = "GAME_STARTING",     // Game is starting (countdown to first frame)
-	WAITING         = "WAITING",           // Game running, waiting between players' frames
-	FRAME_START     = "FRAME_START_DELAY", // Waiting between frame start and roll start
-	ROLL_AWAITING   = "ROLL_AWAITING",     // Waiting for REQUEST_PLAY_ROLL from active player
-	ROLL_PROCESSING = "ROLL_PROCESSING",   // REQUEST_PLAY_ROLL received, waiting for sim to finish
-	ROLL_PLAYBACK   = "ROLL_PLAYBACK",     // Roll keyframes sent, waiting for replay to finish
-	ROLL_END        = "ROLL_END_DELAY",    // Waiting before next roll or frame end
-	FRAME_END       = "FRAME_END_DELAY",   // Waiting before next player's frame
-	GAME_ENDING     = "GAME_ENDING",       // Game is ending
+	NONE            = "NONE",            // Not running / idle
+	GAME_STARTING   = "GAME_STARTING",   // Game is starting (countdown to first frame)
+	WAITING         = "WAITING",         // Game running, waiting between players' frames
+	FRAME_START     = "FRAME_START",     // Waiting between frame start and roll start
+	ROLL_AWAITING   = "ROLL_AWAITING",   // Waiting for REQUEST_PLAY_ROLL from active player
+	ROLL_PROCESSING = "ROLL_PROCESSING", // REQUEST_PLAY_ROLL received, waiting for sim to finish
+	ROLL_PLAYBACK   = "ROLL_PLAYBACK",   // Roll keyframes sent, waiting for replay to finish
+	ROLL_END        = "ROLL_END",        // Waiting before next roll or frame end
+	FRAME_END       = "FRAME_END",       // Waiting before next player's frame
+	GAME_ENDING     = "GAME_ENDING",     // Game is ending
 }
