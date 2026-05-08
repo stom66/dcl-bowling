0ef961bb896949d4d4434db2f323275c3c76eed6
stom <tomsteventon@gmail.com>
2026-05-08 06:42:31 +0100

    feat: add sfx and bgm

--- dcl/src/client/data/sfx.ts
+++ dcl/src/client/data/sfx.ts
@@ -1,16 +1,43 @@
 export const sfx = {
 
 	music: [
-		"assets/sfx/bgm-01.mp3",
+		"assets/sfx/music/music_zapsplat_astro_race.mp3",
 	],
 
 	confirm: [
-		"assets/sfx/confirm_style_5_002.mp3",
-		"assets/sfx/confirm_style_5_001.mp3",
+		"assets/sfx/confirm_style_5_002.wav",
+		"assets/sfx/confirm_style_5_001.wav",
+	],
+
+	bowl_result_bad: [
+		"assets/sfx/confirm_style_3_001.wav"
+	],
+	bowl_result_good: [
+		"assets/sfx/confirm_style_2_echo_001.wav",
+		"assets/sfx/confirm_style_2_echo_002.wav",
+		"assets/sfx/confirm_style_2_echo_003.wav",
+	],
+	bowl_result_great: [
+		"assets/sfx/confirm_style_2_echo_004.wav",
+		"assets/sfx/confirm_style_2_echo_005.wav",
+	],
+
+	click: [
+		"assets/sfx/click-01.wav",
+		"assets/sfx/click-02.wav",
+		"assets/sfx/click-03.wav",
 	],
 
 	warning: [
 		"assets/sfx/error_style_2_001.mp3",
+	],
+
+	swish: [
+		"assets/sfx/swish-01.wav",
+	],
+
+	alert: [
+		"assets/sfx/alert-01.wav",
 	],
 
 	strike: [
