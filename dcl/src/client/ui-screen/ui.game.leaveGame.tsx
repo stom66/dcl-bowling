52eaca6df9fdd9210734cdd721a0df037480ca2f
stom <tomsteventon@gmail.com>
2026-05-12 01:37:51 +0100

    feat: update "Leave game" button

--- dcl/src/client/ui-screen/ui.game.leaveGame.tsx
+++ dcl/src/client/ui-screen/ui.game.leaveGame.tsx
@@ -4,12 +4,9 @@ import { BtnStateUVIndex, buttonUVs } from './utils/btn-utils'
 import { ClientMessaging } from '../clientMessaging'
 import { ClientStore } from '../clientStore'
 import { PlayerStatus } from 'src/shared/enums'
+import { getCanvasInfo } from './utils/sizing'
 
 
-var defaultColor = Color4.fromHexString('#4C958166')
-var hoverColor = Color4.fromHexString('#4C9581FF')
-
-var currentColor = defaultColor
 var buttonState = BtnStateUVIndex.NORMAL
 
 const clientStore = ClientStore.getInstance()
@@ -17,18 +14,19 @@ const clientStore = ClientStore.getInstance()
 // MARK: Main GameUI
 export function LeaveGameUI() {
 	
-	const playerStatus = ClientStore.getInstance().getPlayerStatus()
-	const isInGame     = playerStatus !== PlayerStatus.IDLE 
+	const isInGame = clientStore.getPlayerStatus() !== PlayerStatus.IDLE 
+
+	const canvasInfo = getCanvasInfo()
 
 	return (
 		<UiEntity
 			uiTransform={{
-				positionType : 'absolute',
-				position     : { top: 6, right: 128 },
-				width        : '128',
-				height       : 48,
-				alignContent : 'flex-start',
+				positionType : 'relative',
+				position     : { top: 80, right: (canvasInfo?.width ?? 0) / 2 - 64 },
+				width        : 192,
+				height       : 64,
 				alignItems   : "center",
+				justifyContent: 'space-around',
 				flexDirection: 'row',
 				flexGrow     : 0,
 				flexShrink   : 0,
@@ -51,17 +49,28 @@ export function LeaveGameUI() {
 				console.log("NORMAL")
 				buttonState = BtnStateUVIndex.HOVER 
 			}}}
-			uiText={{
-				value: "Leave Game",
-			}}
 
 			uiBackground={{ 
 				texture    : { src: 'assets/images/ui/btn-primary-atlas-b.png' }, 
 				textureMode: 'stretch',
 				uvs        : buttonUVs[buttonState],
 			}}
-
 		>
+			<UiEntity
+				uiTransform={{
+					width: "75%",
+					height: "75%",
+				}}
+				uiBackground={{
+					texture: { src: 'assets/images/ui/icon-atlas.png' },
+					textureMode: 'stretch',
+					uvs: [
+						0, 0.625, 
+						0, 0.75, 
+						0.5, 0.75, 
+						0.5, 0.625],
+				}}
+			/>
 		</UiEntity>
 	)
 }
